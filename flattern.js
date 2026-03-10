5beb40ffe73b7ab931705772a9063d5d77eac223
Danish <kamal.danish010@gmail.com>
2026-03-10 12:40:46 +0530

    testing

--- flattern.js
+++ flattern.js
@@ -1,18 +1,16 @@
-let array = [1,2,[3,4],[5,[6,7]]];
-let result=[]
-for (let i = 0; i < array.length; i++) {
-    if(Array.isArray(array[i])){
-        for (let j = 0; j < array.length; j++) {
-           if(Array.isArray(array[i][j])){
-            result = result.concat(array[i][j]);
-           }else{
-            result.push(array[i][j]);
-           }
-            
+function flatten(arr, result = []) {
+    for (let i = 0; i < arr.length; i++) {
+
+        if (Array.isArray(arr[i])) {
+            flatten(arr[i], result);
+        } else {
+            result.push(arr[i]);
         }
-    }else{
-        result.push(array[i][j]);
+
     }
-    
+    return result;
 }
-console.log(array)
+
+let array = [1,2,[3,4],[5,[6,7]]];
+
+console.log(flatten(array));
