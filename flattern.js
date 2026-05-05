2705a8175335c5b55f7fabe8c88f15e3a09cd5c4
Danish <kamal.danish010@gmail.com>
2026-05-05 17:21:17 +0530

    Changes

--- flattern.js
+++ flattern.js
@@ -13,3 +13,18 @@ function flatten(arr,result=[]){
 let array = [1,2,[3,4],[5,[6,7]]];
 
 console.log(flatten(array));
+
+
+
+// let array=[1,2,3,4,5,[15],[[[8,91,10]]]];
+// let result=[]
+// function flattern(array){
+// for(let item of array){
+//     if(Array.isArray(item)){
+//         flattern(item)
+//     }else{
+//         result.push(item)
+//     }
+// }2
+// return result
+// }
